8236717b5dbad7a275aee62f1dd0f23a683570fb
Matthias Gattermeier <matthias.gattermeier@gmail.com>
2015-08-03 21:17:32 -0700

    displaying politician names on page. pathetic progress.

--- client/views/PoliticiansView.js
+++ client/views/PoliticiansView.js
@@ -12,14 +12,13 @@ var PoliticiansView = Backbone.View.extend({
   },
 
   render: function() {
-    console.log(' === render triggered === ', this.collection);
     this.collection.forEach(function(model) {
       var item = new PoliticianView({
         model: model
       });
-      var html = item.render();
+      var html = item.render().el;
       console.log(html);
-      this.$el.prepend(html);
+      this.$el.append(html);
     }, this);
     // var html = this.collection.map(function(model) {
     //   var item = new PoliticianView({
