a081bb8ad6b40f16f59e47cca0c4a01c05d9cc00
Matthias Gattermeier <matthias.gattermeier@gmail.com>
2015-08-19 09:44:32 -0700

    Update twitter.js

--- config/twitter.js
+++ config/twitter.js
@@ -1,8 +1,8 @@
 module.exports = {
   twitter: {
-    consumer_key: '2QUgkOBYnJNq4vqnG4mYqRj28',
-    consumer_secret: 'vWWFZY4YFBnsbv6T4h0ZJGDUFUwTtZt08K3GlKiyuL5SGp3XZ3',
-    access_token: '26032565-WeJh68cBIVTUZAjtqNGoFHugCCB2oVXUuxmh8iBOC',
-    access_token_secret: '4zkSx5sjvOIkaW5O60asd8lx89z5ZRJdnZGdww1P5eObm'
+    consumer_key: '',
+    consumer_secret: '',
+    access_token: '',
+    access_token_secret: ''
   }
 };
